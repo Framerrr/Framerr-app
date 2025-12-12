87b0a20a448adb944edebe66196d8fc15b6f08fb
Framerrr <jon.anzalone415@gmail.com>
2025-12-11 22:38:03 -0500

    fix(db): destructure db in all migrated modules for proper import

--- server/db/notifications.js
+++ server/db/notifications.js
@@ -1,4 +1,4 @@
-const db = require('../database/db');
+const { db } = require('../database/db');
 const { v4: uuidv4 } = require('uuid');
 const logger = require('../utils/logger');
 
--- server/db/systemConfig.js
+++ server/db/systemConfig.js
@@ -1,4 +1,4 @@
-const db = require('../database/db');
+const { db } = require('../database/db');
 const logger = require('../utils/logger');
 
 // Default system configuration
