fd1344ff83d705109e75aa7a90bdae475a0ecd91
Framerrr <jon.anzalone415@gmail.com>
2025-12-11 22:31:40 -0500

    fix(migration): destructure db from module exports

--- server/scripts/migrate-to-sqlite.js
+++ server/scripts/migrate-to-sqlite.js
@@ -14,7 +14,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const db = require('../database/db');
+const { db } = require('../database/db');
 
 // Determine data directory
 const DATA_DIR = process.env.DATA_DIR || path.join(__dirname, '..', 'data');
